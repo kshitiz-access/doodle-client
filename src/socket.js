eb75ccf6257ec42f2c4ae515294dec9e0add6754
kshitiz-access <pandey.kshitiz05@gmail.com>
2025-10-15 09:30:00 +0530

    added socket.io client connection

---
+++ src/socket.js
@@ -0,0 +1,3 @@
+import { io } from "socket.io-client";
+const URL = process.env.NODE_ENV === 'production' ? 'https://doodle-p3oj.onrender.com' : 'http://localhost:5000';
+export const socket = io(URL);
