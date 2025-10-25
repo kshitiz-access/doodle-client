edc26dbaea576f35b454ebd6cab45c07aac22922
kshitiz-access <pandey.kshitiz05@gmail.com>
2025-10-26 02:21:28 +0530

    update socket URL to connect to Railway server

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,3 @@
 import { io } from "socket.io-client";
-const URL = process.env.NODE_ENV === 'production' ? 'https://doodle-p3oj.onrender.com' : 'http://localhost:5000';
+const URL = process.env.NODE_ENV === 'production' ? 'https://doodle-server-production.up.railway.app' : 'http://localhost:5000';
 export const socket = io(URL);
